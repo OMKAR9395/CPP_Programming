041c41bea56fffb341e4accda2087918a2de8a60
OMKAR9395 <omkarmohite85@gmail.com>
2025-03-26 00:16:02 +0530

    Caase Study

--- CaseStudy/01 Circle/01Area_Of_Circle.cpp
+++ CaseStudy/01 Circle/01Area_Of_Circle.cpp
@@ -5,29 +5,48 @@ using namespace std;
 class Circle
 {
   public:
+
+      ///   Data Members
       float Radius, Area, Circum;
 
+      ///   Constructors Declaration
       Circle();
       Circle(float Radius);
       Circle(const Circle &cRef);
 
+      /// Destructor
       ~Circle();
 
+      ///     Member Functions
       void Calculate_Area();
       void Calculate_Circumference();
       void Display();
 
   private:
+
+      ///   Constatnt Data Member
       const float PI ;
 };
 
-
-int main()
+int main()      /// Entry point Function
 {
-    Circle Calc(5.5);
-    Calc.Calculate_Area();
-    Calc.Calculate_Circumference();
-    Calc.Display();
+
+    Circle Calc1;
+
+    Calc1.Calculate_Area();  /// function call
+
+    Calc1.Calculate_Circumference(); /// function call
+
+    Calc1.Display(); /// Function call
+
+    Circle Calc(5.5);       /// Object created Parameterized constructor call
+
+    Calc.Calculate_Area();  /// function call
+
+    Calc.Calculate_Circumference(); /// function call
+
+    Calc.Display(); /// Function call
+
     getch();
 
     return 0;
@@ -35,17 +54,20 @@ int main()
 
 Circle::Circle():PI(3.14)
 {
+    cout<< "We Are Inside Of Default Constructor"<< endl;
     Radius = Area = Circum = 0.0;
 }
 
 Circle::Circle(float Rad):PI(3.14)
 {
+    cout<< "We Are Inside Of Parameterized Constructor"<< endl;
     Radius = Rad;
     Area = Circum = 0.0;
 }
 
 Circle::Circle(const Circle &cRef):PI(3.14)
 {
+    cout<< "We Are Inside Of Copy Constructor"<< endl;
     Radius = cRef.Radius;
     Area = cRef.Area;
     Circum = cRef.Circum;
@@ -53,6 +75,7 @@ Circle::Circle(const Circle &cRef):PI(3.14)
 
 Circle::~Circle()
 {
+    cout<< "We Are Inside Of Destructor"<< endl;
    system("cls");
    cout<<"Thanks For Using This Application"<<endl;
 }
